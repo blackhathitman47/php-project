05c24833ce88241cb420f7d3cfb6695b78cf18e1
blackhathitman47 <noureddine0elmouden@gmail.com>
2018-05-23 01:28:11 +0000

    web

--- Contact Us.php
+++ Contact Us.php
@@ -10,71 +10,7 @@
 	</head>
 <body>
 	<header class="site-header-wrap">
-		<header class="row  top-bar">
-			<div class="inl-bloc">
-				<i class="fa fa-home" aria-hidden="true"></i>
-				<p class="p-color-font inl-bloc">Hay Moulay Abdellah, Rue 295 N°1, Sur Boulvars Mouhamed 6, Casablanca</p>
-			</div>
-			<div class="fl-right inl-bloc">
-				<p class="p-color-font inl-bloc">Follow us : </p>
-				<a href="https://www.facebook.com/Multi-Parts-Auto-171881533518473/?ti=as" target="_blank"><i class="fa fa-facebook-official" aria-hidden="true"></i></a>
-			</div>
-		</header>
-		<header class="clear bar">
-			<img src="images/Multi-Parts (Final).jpg">
-			<div id="div-info" class="fl-right inl-bloc">
-				<div class=" inl-bloc info-wrap">
-					<i class="fa fa-comments-o fa-4x" aria-hidden="true"></i>
-					<p class="info-p inl-bloc">+212 000 000 000
-					<br>contact@gmail.com</p>
-				</div>
-				<div class=" inl-bloc info-wrap">
-					<i class="fa fa-clock-o fa-4x" aria-hidden="true"></i>
-					<p class="info-p inl-bloc"> 8:00 - 17:30
-					<br>Monday to Saturday</p>
-				</div>
-			</div>
-		</header>
-		<header id="nav" class="clear bar nav-color">
-			<nav class="navbar navbar-inverse navmod">
-				<div class="container">
-					<div class="navbar-header">
-						<button type="button" class="navbar-toggle collapsed btt-colore " data-toggle="collapse" data-target=".navbar-collapse">
-							<span class="sr-only">Toggle navigation</span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-						</button>
-						<a class="navbar-brand" href="index.html">Multi-Parts Auto</a>
-					</div>
-					<div class="navbar-collapse collapse" aria-expanded="false" style="height: 0.8px;">
-						<ul class="nav navbar-nav ">
-							<li class="active"><a class="bg-active" href="index.html">Home</a></li>
-				 		    <li><a href="About Us.html">About Us</a></li>
-						    <li><a href="contact us.html">Contact</a></li>
-							<li class="dropdown">
-								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Products <span class="caret"></span></a>
-								<ul class="dropdown-menu">
-									<li><a href="#">Engine Parts</a></li>
-									<li role="separator" class="divider"></li>
-									<li><a href="#">Transmission</a></li>
-									<li role="separator" class="divider"></li>
-									<li><a href="#">Electrical Part</a></li>
-									<li role="separator" class="divider"></li>
-									<li><a href="#">Part Suspension</a></li>
-									<li role="separator" class="divider"></li>
-									<li><a href="#">Lubricant + Antifreeze And Additive</a></li>
-									<li role="separator" class="divider"></li>
-									<li><a href="#">Lubricating Oil</a></li>
-									<li role="separator" class="divider"></li>
-									<li><a href="#">Other</a></li>
-								</ul>
-							</li>
-						</ul>
-					 </div><!--/.nav-collapse -->
-				</div>
-			</nav>
-		</header>
+	<?php include'php/nav.php'; ?>
 	</header>
 	<main>
 			<section id="section1">
@@ -160,44 +96,6 @@
 				</div>
 			</section>
 		</main>
-		<footer class="footer-distributed">
-				<div class="footer-left">
-	
-					<h3>Multi<span>-Parts</span> Auto</h3>
-	
-					<p class="footer-links">
-						<a href="index.html">Home</a>
-						·
-						<a href="About Us.html">About Us</a>
-						·
-						<a href="#">Products</a>
-						·
-						<a href="#">Gallery</a>
-					</p>
-	
-					<p class="footer-company-name">Copyright.Talon &copy; 2018</p>
-	
-					<div class="footer-icons">
-	
-						<a href="#"><i class="fa fa-facebook"></i></a>
-						<a href="#"><i class="fa fa-google-plus"></i></a>
-	
-					</div>
-	
-				</div>
-				<div class="footer-right">
-	
-					<p>Contact Us</p>
-	
-					<form action="#" method="post">
-	
-						<input type="text" name="email" placeholder="Email" />
-						<textarea name="message" placeholder="Message"></textarea>
-						<button>Send</button>
-	
-					</form>
-	
-				</div>
-			</footer>
+	<?php include'php/footer.php'; ?>
 </body>
 </html>
---
+++ php/footer.php
@@ -0,0 +1,39 @@
+<footer class="footer-distributed">
+			<div class="footer-left">
+
+				<h3>Multi<span>-Parts</span> Auto</h3>
+
+				<p class="footer-links">
+					<a href="index.php">Home</a>
+					·
+					<a href="About Us.php">About Us</a>
+					·
+					<a href="#">Products</a>
+					·
+					<a href="#">Gallery</a>
+				</p>
+
+				<p class="footer-company-name">Copyright.Talon &copy; 2018</p>
+
+				<div class="footer-icons">
+
+					<a href="#"><i class="fa fa-facebook"></i></a>
+					<a href="#"><i class="fa fa-google-plus"></i></a>
+
+				</div>
+
+			</div>
+			<div class="footer-right">
+
+				<p>Contact Us</p>
+
+				<form action="#" method="post">
+
+					<input type="text" name="email" placeholder="Email" />
+					<textarea name="message" placeholder="Message"></textarea>
+					<button>Send</button>
+
+				</form>
+
+			</div>
+		</footer>
